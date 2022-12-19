5b4c490cb21cebe326e24319a1f5d665acca90bf
jcharlesl <joshclopes@gmail.com>
2022-12-18 21:30:35 -0500

    Added splash page, with button going to DB

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -9,16 +9,19 @@ app.config['MYSQL_PASSWORD']='hellosql'
 app.config['MYSQL_DB']='hellodb'
 socket = "winter-dynamics-371520:northamerica-northeast2:hellosql"
 app.config['MYSQL_UNIX_SOCKET']=f'''/cloudsql/{socket}''' 
-
 mysql = MySQL(app)
+DB = 'FFEF_A'
 
 @app.route ('/')
 def form():
     return render_template('form.html')
 
+@app.route ('/landing')
+def landingform():
+    return render_template('landing.html')
+
 @app.route('/login', methods= ['POST','GET'])
 def login():
-    DB = 'FFEF_A'
     if request.method == 'GET':
 	    return 'LOG IN PROPERLY PLEASE.'
     if request.method == 'POST':
@@ -31,7 +34,17 @@ def login():
         cur.execute(f'''INSERT INTO {DB} VALUES (%s,%s,%s,%s,%s)''',(first_name,last_name,email,choice,donate))
         mysql.connection.commit()
         cur.close()
-        return ('/')
-        
+        return redirect('/landing')
+
+@app.route('/landing', methods= ['POST','GET'])
+def landing():
+    if request.method == 'GET':
+	    return redirect ('/')
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        cur.execute(f'''SELECT * FROM {DB}''')
+        rv = cur.fetchall()
+        return str(rv)
+
 if __name__=='__main__':
 	app.run(debug=True)
